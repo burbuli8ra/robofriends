c8788cfe03cfabb0fd3a47fe428aab3c45a3f88e
Marilena Ioannou <marilena.ioannou.sg@gmail.com>
2019-01-24 07:20:04 +0200

    Implementation of middlewares & async actions
    * Addition of redux logger to display logs
    * Conversion of fetch call to async action

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { setSearchField } from '../actions';
-import { apiCall } from "../api/api";
+import { setSearchField, requestRobots } from '../actions';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
@@ -10,40 +9,33 @@ import '../styles/App.css';
 
 const mapStateToProps = state => {
   return {
-  	// If we had more than stores, we would have to specify the store as well
-    // searchField: state.searchRobots.searchField
-    searchField: state.searchField
+  	searchField: state.searchRobots.searchField,
+    isPending: state.requestRobots.isPending,
+    robots: state.requestRobots.robots,
+    error: state.requestRobots.error
   }
 };
 
 // Triggers the action
 const mapDispatchToProps = dispatch => {
   return {
-    onSearchChange: ({ target: {value} }) => dispatch(setSearchField(value))
+    onSearchChange: ({ target: {value} }) => dispatch(setSearchField(value)),
+    onRobotsRequest: () => dispatch(requestRobots())
   }
 };
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: []
-    };
-  }
-
   componentDidMount() {
-    apiCall('https://jsonplaceholder.typicode.com/users')
-      .then(users => {this.setState({ robots: users })});
+    this.props.onRobotsRequest();
   }
 
   render() {
-    const { robots } = this.state;
-    const { searchField, onSearchChange } = this.props;
+    const { searchField, onSearchChange, robots, isPending } = this.props;
     const filterRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchField.toLowerCase())
     });
 
-    return !robots.length ?
+    return isPending ?
       <h1 className='f2 tc'>Loading...</h1> :
       (
         <div className='tc'>
