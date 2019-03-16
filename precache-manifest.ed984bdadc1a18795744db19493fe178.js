774b974a5750e45e9ebfe2515c4f7104ae44908f
Marilena Ioannou <marilena.ioannou.sg@gmail.com>
2019-03-16 07:34:14 +0200

    Updates

---
+++ precache-manifest.ed984bdadc1a18795744db19493fe178.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "0176d6388520866a7690",
+    "url": "/robofriends/static/js/main.1082d5b6.chunk.js"
+  },
+  {
+    "revision": "04d3640118008cc3e4e2",
+    "url": "/robofriends/static/js/2.21f4a9ea.chunk.js"
+  },
+  {
+    "revision": "0176d6388520866a7690",
+    "url": "/robofriends/static/css/main.f87d2893.chunk.css"
+  },
+  {
+    "revision": "04d3640118008cc3e4e2",
+    "url": "/robofriends/static/css/2.bb5ddab2.chunk.css"
+  },
+  {
+    "revision": "775c080019a8f6c51d74ff3c9247e944",
+    "url": "/robofriends/index.html"
+  }
+];
